18648858a50fdd8264bd4f1d442e6d482996d803
slayme <552401150@qq.com>
2025-05-21 15:54:13 +0900

    Add NavBar component and adjust layout styles#

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import MyCardList from "./components/MyCardList";
+import NavBar from "./modules/NavBar.jsx";
+import './modules/NavBar.css';
 const App = () => {
   return (
     <div>
+      <NavBar />
       <h1>个性卡片展示</h1>
       <MyCardList />
     </div>
--- src/modules/NavBar.jsx
+++ src/modules/NavBar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import './NavBar.css';
 const NavBar=()=>{
-return
+return(
+    <nav className="NavBarContainer">
+    <h3 className="NavBarSmallBox">主页</h3>
+    <h3 className="NavBarSmallBox">添加卡片</h3>
+    <h3 className="NavBarSmallBox">我的卡片</h3>
+    </nav>
+)
 }
+export default NavBar;
